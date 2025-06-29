fa0173bf89e493d945e93855ca554310a2ac0adb
arit-pal <itsaritpal@gmail.com>
2025-06-29 12:19:51 +0530

    add: about page structure

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,29 @@
+import "./About.css";
+
 const About = () => {
     return (
-        <>
-            <h1>About Us</h1>
-        </>
+        <div className="about-container">
+            <header className="header-content">
+                <div className="header-name">
+                    <p>Arit Pal</p>
+                </div>
+                <div className="header-links">
+                    <div className="header-about">
+                        <p>About Me</p>
+                    </div>
+                    <div className="header-contact">
+                        <p>Contact</p>
+                    </div>
+                </div>
+            </header>
+            <main className="main-content">
+                <h1>About Me</h1>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+            </main>
+            <footer className="footer-content">
+                <p>© 2025 Arit Pal. All rights reserved.</p>
+            </footer>
+        </div>
     );
 };
 
